a4c56973b1dd64bb5437c4cb234ebf9af5ff0600
vyasmax9-cloud <vyasmax9@gmail.com>
2026-05-31 20:36:26 +0530

    Update hosting.py

--- Predictive_Maintenance/hosting/hosting.py
+++ Predictive_Maintenance/hosting/hosting.py
@@ -1,29 +1,34 @@
-
-from huggingface_hub import (
-    login,
-    upload_folder
-)
-
+from huggingface_hub import login, upload_folder
 import os
 
-HF_TOKEN = os.getenv(
-    "HF_TOKEN"
-)
+# ==========================================
+# HF TOKEN
+# ==========================================
 
-login(
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+if not HF_TOKEN:
+    raise Exception("HF_TOKEN not found")
+
+HF_TOKEN = HF_TOKEN.strip()
+
+print("HF Token Loaded Successfully")
+
+# ==========================================
+# LOGIN
+# ==========================================
+
+login(token=HF_TOKEN)
+
+# ==========================================
+# UPLOAD TO HUGGING FACE SPACE
+# ==========================================
+
+upload_folder(
+    repo_id="vyasmax9/predictive-maintenance",
+    folder_path="Predictive_Maintenance",
+    repo_type="space",
     token=HF_TOKEN
 )
 
-upload_folder(
-
-    repo_id="vyasmax9/predictive-maintenance",
-
-    folder_path="Predictive_Maintenance",
-
-    repo_type="space"
-
-)
-
-print(
-    "Deployment Uploaded Successfully"
-)
+print("Deployment Uploaded Successfully")
